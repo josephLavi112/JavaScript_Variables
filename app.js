0c85a79aed3c23594d4aff866589eac8d3744249
Joseph LaVia <josephdlavia@gmail.com>
2023-06-21 16:52:12 -0500

    JavaScriptVarXercise

--- app.js
+++ app.js
@@ -4,7 +4,31 @@ console.log(
 );
 
 // Exercise 1
+let lastname;
+var  age;
 
 // Exercise 2
+const firstName = "Joe";
+lastname = "LaVia";
+age = 25;
 
 // Exercise 3
+let language = "javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+//Exercise 4
+let price = 20.00;
+let isOnSale = false;
+let salesPercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+//Exercise 5
+let title = "Name of the wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
